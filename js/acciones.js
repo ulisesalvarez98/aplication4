f27c8ade994b00303abf3e6486d2c135c80b5eda
ulises alvarez corio <ulisesalvarezcorio3h@gmail.com>
2016-04-18 14:21:37 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -2,11 +2,11 @@
 
 $(document).ready(function(e) {
     document .addEventListener("deviceready" ,function(){
-		$('#dip table td').eq(3).text(device.model);
-		$('#dip table td').eq(5).text(device.cordova);
-		$('#dip table td').eq(7).text(device.platform);
-		$('#dip table td').eq(9).text(device.version);
-		$('#dip table td').eq(11).text(device.uuid);
+		$('#disp table td').eq(3).text(device.model);
+		$('#disp table td').eq(5).text(device.cordova);
+		$('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
 	document.addEventListener("pause",function(){
 		escribehistoria('la app se pauso');
 	},false);//pause
